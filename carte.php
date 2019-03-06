08b6eec2ff5352a31646b96e6be7452ea4f50c7b
Sygix <admin@sygix.fr>
2019-03-06 09:53:03 +0100

    Adding Lea's part

---
+++ carte.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="fr">
+	<head> 
+		<title>Cartes</title>
+		<meta charset="utf-8" />
+		<link rel="stylesheet" type="text/css" href="style/style2.css" />
+	</head> 
+	<body>  
+		<div class="carte">
+			<h1 class="titre1">LES CARTES</h1>
+				<div class="bloc1">
+					<div class="carte1">
+						<p>carte 1</p>
+					</div>
+					<div class="carte2">
+						<p>carte 2</p>
+					</div>
+					<div class="carte3">
+						<p>carte 3</p>
+					</div>
+					<div class="carte4">
+						<p>carte 4</p>
+					</div>
+					<div class="carte5">
+						<p>carte 5</p>
+					</div>
+				</div>
+				<div class="bloc2">
+					<div class="carte6">
+						<p>carte 6</p>
+					</div>
+					<div class="carte7">
+						<p>carte 7</p>
+					</div>
+					<div class="carte8">
+						<p>carte 8</p>
+					</div>
+					<div class="carte9">
+						<p>carte 9</p>
+					</div>
+					<div class="carte10">
+						<p>carte 10</p>
+					</div>
+				</div>
+
+			<a class="lien1" href="#">Tout voir</a>
+		</div>
+
+		<div class="plateau">
+			<h1 class="titre2">LE PLATEAU</h1>
+				<div class="blocplateau">
+					<p>Plateau</p>
+				</div>
+
+				<p class="texte1">Inter quos Paulus eminebat notarius ortus in Hispania, glabro quidam sub vultu latens, odorandi vias periculorum occultas perquam sagax. is in Brittanniam missus ut militares quosdam perduceret ausos conspirasse Magnentio, cum reniti non possent, iussa licentius supergressus fluminis modo fortunis conplurium sese repentinus infudit et ferebatur per strages multiplices ac ruinas, vinculis membra ingenuorum adfligens et quosdam obterens manicis, crimina scilicet multa consarcinando a veritate longe discreta. unde admissum est facinus impium, quod Constanti tempus nota inusserat sempiterna.</p>
+
+				<a class="lien1" href="#">En savoir plus</a>
+		</div>
+
+		<div class="deck">
+			<h1 class="titre3">LE DECK</h1>
+				<div class="blocdeck">
+					
+				</div>
+
+				<p class="texte2">Inter quos Paulus eminebat notarius ortus in Hispania, glabro quidam sub vultu latens, odorandi vias periculorum occultas perquam sagax. is in Brittanniam missus ut militares quosdam perduceret ausos conspirasse Magnentio, cum reniti non possent, iussa licentius supergressus fluminis modo fortunis conplurium sese repentinus infudit et ferebatur per strages multiplices ac ruinas, vinculis membra ingenuorum adfligens et quosdam obterens manicis, crimina scilicet multa consarcinando a veritate longe discreta. unde admissum est facinus impium, quod Constanti tempus nota inusserat sempiterna.Inter quos Paulus eminebat notarius ortus in Hispania, glabro quidam sub vultu latens, odorandi vias periculorum occultas perquam sagax. is in Brittanniam missus ut militares quosdam perduceret ausos conspirasse Magnentio, cum reniti non possent, iussa licentius supergressus fluminis modo fortunis conplurium sese repentinus infudit et ferebatur per strages multiplices ac ruinas, vinculis membra ingenuorum adfligens et quosdam obterens manicis, crimina scilicet multa consarcinando a veritate longe discreta. unde admissum est facinus impium, quod Constanti tempus nota inusserat sempiterna.<br/><br/>
+
+				<a class="lien2" href="#">En savoir plus</a></p>
+
+		</div>
+	</body>  
+</html>
