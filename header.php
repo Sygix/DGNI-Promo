dd0402a0e938d54b540d2dca403bce915590fc0f
Sygix <admin@sygix.fr>
2019-03-19 17:49:02 +0100

    Added Store page + CSS loading + some changes

--- header.php
+++ header.php
@@ -5,8 +5,9 @@
     <title>Triumph</title>
     <meta name="viewport" content="width=device-width, user-scalable=no">
     <link rel="stylesheet" type="text/css" href="style/main.css">
-    <link rel="stylesheet" type="text/css" href="style/history.css">
-    <link rel="stylesheet" type="text/css" href="style/cartes.css">
+    <?php
+    echo '<link rel="stylesheet" type="text/css" href="style/'.basename($_SERVER['PHP_SELF'], ".php").'.css">';
+    ?>
     <link rel="icon" href="images/crown.png" />
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 </head>
@@ -15,13 +16,13 @@
 <nav class="nav">
     <div id="nav-logo-container"><a href="index.php"><img id="nav-logo" src="images/crown.png"></a></div>
     <ul id="nav-center">
-        <a href="http://89.234.183.130/jeu"><li>
+        <a href="deck.php"><li>
             Le Jeu
         </li></a>
-        <a href="#"><li>
+        <a href="news.php"><li>
             Actualités
         </li></a>
-        <a href="#"><li>
+        <a href="store.php"><li>
             La Boutique
         </li></a>
     </ul>
---
+++ store.php
@@ -0,0 +1,29 @@
+<!-- Header -->
+<?php require_once "header.php" ?>
+<!-- Nav -->
+<div class="header-container-background">
+    <header class="header-container">
+        <img src="images/logo3D2.png"
+    </header>
+</div>
+<!-- Header -->
+<div id="page-store">
+    <div class="title">
+        <div class="arrow-right"></div>
+        <h1 class="title-font">Nos produits</h1>
+        <div class="arrow-left"></div>
+    </div>
+    <div id="products">
+        <div id="top"><div id="image-plateau"><div class="overlay-shop">Plateau</div></div></div>
+        <div id="bottom">
+            <div id="left"><div class="overlay-shop">Deck Nordique</div></div>
+            <div id="right"><div class="overlay-shop">Deck Grecque</div></div>
+        </div>
+    </div>
+    <a href="#" id="button-en-savoir" class="button1 meet draw">Finaliser</a>
+</div>
+<!-- Footer -->
+<?php require_once "footer.php" ?>
+<!-- Footer -->
+</body>
+</html>
